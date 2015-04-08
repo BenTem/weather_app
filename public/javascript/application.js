1f58bd58746ca232016a449cc453216532c6d736
Ben <benctemple@gmail.com>
2015-04-08 13:47:15 -0700

    functioning ajax call

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#srch_btn").on('click',function(){
+    console.log($("#search").val());
+    $.getJSON( 
+      "http://autocomplete.wunderground.com/aq?query=" + $('#search').val() + '&cb=?'
+      ).done(function(returnData){
+        // console.log(returnData);
+        var result = returnData.RESULTS
+        // console.log(result)
+        $("#cities").empty();
+        $.each(result, function(i, value) {
+          $("#cities").append( "<p>"  + value.name + "</p>");
+        })
+      }); 
+  });
+
 });
