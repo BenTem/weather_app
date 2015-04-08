2409aa719576fdb70404ab1f5f1245d6399d7ea4
Ben <benctemple@gmail.com>
2015-04-08 15:59:52 -0700

    autocomplete functionality

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,18 +1,59 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  $("#srch_btn").on('click',function(){
-    console.log($("#search").val());
-    $.getJSON( 
-      "http://autocomplete.wunderground.com/aq?query=" + $('#search').val() + '&cb=?'
-      ).done(function(returnData){
-        // console.log(returnData);
-        var result = returnData.RESULTS
-        // console.log(result)
-        $("#cities").empty();
-        $.each(result, function(i, value) {
-          $("#cities").append( "<p>"  + value.name + "</p>");
-        })
-      }); 
-  });
+//   $("#srch_btn").on('click',function(){
+//     console.log($("#search").val());
+//     $.getJSON( 
+//       "http://autocomplete.wunderground.com/aq?query=" + $('#search').val() + '&cb=?'
+//       ).done(function(returnData){
+//         var result = returnData.RESULTS
+//         $("#cities").empty();
+//         $.each(result, function(i, value) {
+//           $("#cities").append( "<p>"  + value.name + "</p>");
+//         })
+//       }); 
+//   });
 
-});
+// });
+
+// $(document).ready(function() {
+
+//   $( "#srch_btn" ).autocomplete({
+//     source: function( request, response ) {
+//     $.getJSON( 
+//       "http://autocomplete.wunderground.com/aq?query=" + $('#search').val() + '&cb=?'
+//       ).done(function( data ) {
+//         response( data );
+//       });
+//     }
+//   });
+// });
+
+// $(function() {
+//   var availableTags = [
+//     "ActionScript",
+//     "AppleScript",
+//     "Asp",
+//     "BASIC",
+//     "C",
+//     "C++",
+//     "Clojure",
+//     "COBOL",
+//     "ColdFusion",
+//     "Erlang",
+//     "Fortran",
+//     "Groovy",
+//     "Haskell",
+//     "Java",
+//     "JavaScript",
+//     "Lisp",
+//     "Perl",
+//     "PHP",
+//     "Python",
+//     "Ruby",
+//     "Scala",
+//     "Scheme"
+//   ];
+//   $( "#search" ).autocomplete({
+//     source: availableTags
+//   });
+// });
